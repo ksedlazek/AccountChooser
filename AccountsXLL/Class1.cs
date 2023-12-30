1d656fbc247329f000fe357aa6b9dbc04db6ea36
Kurt Sedlazek <ksedlazek@gmail.com>
2023-12-30 13:38:47 -0500

    fixed functions, add lookup

--- AccountsXLL/Class1.cs
+++ AccountsXLL/Class1.cs
@@ -1,6 +1,12 @@
 ﻿using AccountChooser;
 using ExcelDna.Integration;
 using Microsoft.ML;
+using System.Formats.Asn1;
+using System.Globalization;
+using System;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace AccountsXLL
 {
@@ -8,33 +14,58 @@ namespace AccountsXLL
     {
         private static ITransformer _model;
         private static MLContext _mlContext;
+        private static IList<Map> _mappings = new List<Map>(); 
+
+        private class Map
+        {
+            public string Description { get; set; } = "";
+            public string Type { get; set; } = "";
+            public string Category { get; set; } = "";
+            public string Account { get; set; } = "";
+        }
 
         static MyFunctions()
         {
             _mlContext = new MLContext(seed: 1);
-            var modelPath = "C:/Workspace/AccountChooser/model.zip";
+            var modelPath = "C:/Workspace/AccountChooser/Data/model.zip";
             _model = _mlContext.Model.Load(modelPath, out var modelInputSchema);
         }
 
         [ExcelFunction(Description = "My first .NET function")]
         public static string SayHello(string name)
         {
-            var predEngine = _mlContext.Model.CreatePredictionEngine<TransactionData, ModelOutput>(_model);
-            var tx = new TransactionData { Description = name };
-            var prediction = predEngine.Predict(tx);
-            return prediction.FullName;
+            return "Hello " + name;
         }
 
         [ExcelFunction(Description = "Load specific mapping values")]
-        public static string LoadMap(string description, float amount, string category)
+        public static string LoadMap(string filename) 
         {
+            var configPersons = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true
+            };
+            using (var reader = new StreamReader(filename))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Map>();
+                _mappings = records.ToList();
+            }
+            return $"Loaded {_mappings.Count} mappings";
+        }
+
+        [ExcelFunction(Description = "Checks for a strict lookup")]
+        public static string LookupAccount(string description, double amount, string category, string type)
+        {
+            var map = _mappings.FirstOrDefault(x => x.Description == description && x.Category == category);
+            if (map != null) return map.Account;
+            return "";
         }
 
         [ExcelFunction(Description = "Predicts the account name based on the trained model")]
-        public static string PredictAccount(string description, float amount, string category)
+        public static string PredictAccount(string description, double amount, string category)
         {
             var predEngine = _mlContext.Model.CreatePredictionEngine<TransactionData, ModelOutput>(_model);
-            var tx = new TransactionData { Description = description, Category = category, Amount = amount };
+            var tx = new TransactionData { Description = description, Category = category, Amount = (float)amount };
             var prediction = predEngine.Predict(tx);
             return prediction.FullName;
         }
